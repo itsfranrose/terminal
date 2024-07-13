fb5c7a686a7d2790649f5550b82cebd2111aabdc
Mehdi Rafee <mrafee113@gmail.com>
2024-07-13 08:51:34 +0330

    add -g for alias man

--- aliases/shell.zsh
+++ aliases/shell.zsh
@@ -3,7 +3,7 @@
 # TODO: add aliases for file/dir/disk space usage
 alias xclip-board="xclip -selection clipboard"
 alias grep='grep --color=auto'
-alias man="$HOME/.local/bin/batman-s --wrap=never --paging=always"
+alias -g man="$HOME/.local/bin/batman-s --wrap=never --paging=always"
 
 alias ipy="python3 -c 'import IPython;
 IPython.terminal.ipapp.launch_new_instance()'"
